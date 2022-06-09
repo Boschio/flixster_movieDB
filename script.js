4326a5575c4cd5792a05c763f87a3138241df490
Bosch <boschio91@gmail.com>
2022-06-09 17:19:25 -0400

    Added Search functionality. Made changes to element and function names.

--- script.js
+++ script.js
@@ -3,47 +3,73 @@ const API_KEY = '3ecab21081541b994be53f6076bcf782';
 
 // Variables
 let movieForm = document.getElementById("form");
-let movieArea = document.querySelector(".movies-area")
+let moviesGrid = document.querySelector("#movies-grid")
 let button = document.getElementById("search");
-let moreButton = document.getElementById("loadBtn");
+let moreButton = document.getElementById("load-more-movies-btn");
+let closeButton = document.getElementById("close-search-btn");
 let results;
 let page = 1;
-
-// pages can be ++ whenever load more button is pressed
+let x = true;
 
 movieForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    movieArea.innerHTML = ``;
+    moviesGrid.innerHTML = ``;
     results = document.querySelector('input').value;
     page = 1;
-    getResults(results);
-    moreButton.style.display = "flex";
+    searchMovies(results);
+    closeButton.style.display = "flex";
 });
 
 moreButton.addEventListener("click", () => {
     page++;
-    getResults(results);
-
+    if (x) {
+        loadNowPlaying(results);
+    } else {
+        searchMovies(results);
+    }
 });
 
-async function getResults(event) {
-    let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`);
-    console.log(response);
+closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    moviesGrid.innerHTML = ``;
+    page = 1;
+    loadNowPlaying();
+    closeButton.style.display = "none";
+})
+
+async function loadNowPlaying(event) {
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=${page}`);
+    // console.log(response);
     let responseData = await response.json();
-    console.log(responseData);
+    // console.log(responseData);
 
     for (let i=0;i<responseData.results.length;i++) {
         displayResults(responseData.results[i]);
     }
 }
 
+async function searchMovies(event) {
+    x = false;
+    console.log(event)
+    let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${page}&include_adult=false&query=${event}`);
+    console.log(response);
+    let responseData = await response.json();
+    console.log(responseData);
+    for (let i=0;i<responseData.results.length;i++) {
+        displayResults(responseData.results[i]);
+    }
+}
+
 function displayResults(e) {
-    movieArea.innerHTML += `
-        <div>
-            
-            <img id="poster" src="http://image.tmdb.org/t/p/w500/${e.poster_path}">
-            <p id = "rating">${e.vote_average}</p>
-            <p id = "title">${e.title}</p>
+    moviesGrid.innerHTML += `
+        <div class="movie-card">
+            <img class="movie-poster" src="http://image.tmdb.org/t/p/w500${e.poster_path}">
+            <p class="movie-votes">${e.vote_average}</p>
+            <p class="movie-title">${e.title}</p>
         </div>
     `;
+}
+
+window.onload = function () {
+    loadNowPlaying();
 }
