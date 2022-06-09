6167b00dd70123854ca0f1b9fd320729ff9daa98
Bosch <boschio91@gmail.com>
2022-06-09 13:26:00 -0400

    Skeleton made for project. tMDB API implemented and in testing.

---
+++ script.js
@@ -0,0 +1,43 @@
+// GLOBAL CONSTANTS
+const API_KEY = '3ecab21081541b994be53f6076bcf782';
+
+// Variables
+let movieForm = document.getElementById("form");
+let movieArea = document.querySelector(".movies-area")
+let button = document.getElementById("search");
+let moreButton = document.getElementById("loadBtn");
+let results;
+
+// pages can be ++ whenever load more button is pressed
+
+movieForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    movieArea.innerHTML = ``;
+    results = document.querySelector('input').value;
+
+    getResults(results);
+    moreButton.style.display = "flex";
+});
+
+moreButton.addEventListener("click", () => {
+
+    getResults(results);
+
+});
+
+async function getResults(event) {
+    let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    console.log(response);
+    let responseData = await response.json();
+    console.log(responseData);
+    //responseData.results.foreach((e) => {
+        displayResults(responseData.results[1]);
+    //})
+}
+
+function displayResults(e) {
+    movieArea.innerHTML += `
+        <p>${e.title}"</p>
+        <img id="test" src="http://127.0.0.1:5500/index.html?GIFs=${e.poster_path}">
+    `;
+}
